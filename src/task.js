7478fd30760619fcddd5abf5fc7ffce98a389b3f
wolecodes <opeyemioyeyemi141@gmail.com>
2024-10-01 11:29:57 +0100

    created a function to update task

--- src/task.js
+++ src/task.js
@@ -1,5 +1,5 @@
 import { getTaskDB, saveTaskDB, inserTaskDB } from "./taskDB";
-import { generateNextId } from "./utils";
+import { generateNextId, colors } from "./utils";
 // Add, Update, and Delete tasks
 // Mark a task as in progress or done
 // List all tasks
@@ -8,11 +8,14 @@ import { generateNextId } from "./utils";
 // List all tasks that are in progress
 
 //add task to database
-
-const addNewTask = async (task) => {
+/*
+ * addNewTask: this function adds a new task to the database
+ * @param {string} - task
+ */
+export const addNewTask = async (task) => {
   const newTask = {
     id: generateNextId(task),
-    description: [],
+    description: task,
     completed: false,
     inProgress: false,
     createdAt: new Date().toISOString(),
@@ -22,4 +25,41 @@ const addNewTask = async (task) => {
   await inserTaskDB(newTask);
 
   return newTask;
+};
+
+// get all task from the database
+export const getAlltask = async () => {
+  const task = await getTaskDB();
+  return task.description;
+};
+
+//update task in database
+/*
+ * updateTask: update the task in the database
+ * @param {number} - id of the task
+ * @param {string} - currnet task description
+ */
+export const updateTask = async (id, task) => {
+  const task = await getAlltask();
+  const currentTaskID = task.find((task) => task.id === +id);
+
+  if (currentTaskID) {
+    task.description = task;
+    task.updatedAt = new Date().toISOString();
+    await saveTaskDB(task);
+    console.log(`${colors.green}Task with ID ${id} updated.${colors.reset}`);
+  } else {
+    console.log(`${colors.red}Task with ID ${id} not found.${colors.reset}`);
+  }
+};
+
+//delete task from database
+/*
+ * deleteTask: delete the task from the database
+ * @param {number} - id of the task
+ */
+
+export const deleteTask = async (id) => {
+  const task = await getAlltask();
+  let filteredTask = task.filter((task) => task.id !== +id);
 };
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,14 @@
 //generate next id for task
+
+//color output
+export const colors = {
+  reset: "\x1b[0m",
+  green: "\x1b[32m",
+  red: "\x1b[31m",
+  yellow: "\x1b[33m",
+  cyan: "\x1b[36m",
+};
+
 /*
  * generateNextId : this function generates the next id for the task
  * @param {Array} - task
@@ -8,4 +18,3 @@ export const generateNextId = (task) => {
   const ids = task.map((task) => task.id);
   return ids.length ? Math.max(...ids) + 1 : 1;
 };
-
